c674bd0bc0a59f094974946376685b77d8e1b3c4
Ioannis Bekiaris <johnbekiaris13@gmail.com>
2024-06-07 13:16:03 +0200

    fixed header bug

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://bekathunder215.github.io',
+  homepage: 'https://bekathunder215.github.io/mywebsite/',
   title: 'JS.',
 }
 
