a23db25890bc2abe602f3ea3e8aacd6273eea841
Bekathunder215 <johnbekiaris13@gmail.com>
2024-11-11 16:10:47 +0100

    fixed the cv viewer

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,30 +1,37 @@
-import { contact } from '../../portfolio'
-import './Contact.css'
+import { contact } from "../../portfolio";
+import "./Contact.css";
 
 const Contact = () => {
-  if (!contact.email) return null
+  if (!contact.email) return null;
 
   return (
-    <section className='section contact center' id='contact'>
-      <h2 className='section__title'>Contact</h2>
+    <section className="section contact center" id="contact">
+      <h2 className="section__title">Contact</h2>
       <a href={`mailto:${contact.email}`}>
-        <span type='button' className='btn btn--outline'>
+        <span type="button" className="btn btn--outline">
           Email me
         </span>
       </a>
-      <br/>
-      Call or Text me <br/>
+      <br />
+      Call or Text me <br />
       <a href={`call:${contact.sms}`}>
-        <span type='button' className='btn btn--outline'>
+        <span type="button" className="btn btn--outline">
           {contact.sms}
         </span>
       </a>
-      <br/>
+      <br />
       <h2>View My CV</h2>
       <p>You can View my CV in PDF format below:</p>
-      <a href="mywebsite/IoannisBekiarisCV.pdf" target="_blank" rel="noopener noreferrer" className='btn btn--outline'>View CV</a>
+      <a
+        href="/IoannisBekiarisCV.pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="btn btn--outline"
+      >
+        View CV
+      </a>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
